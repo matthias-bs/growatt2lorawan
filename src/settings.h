031aa2f334c6db703b931b30e5b86da222be773d
Matthias Prinke <83612361+matthias-bs@users.noreply.github.com>
2023-04-17 21:01:24 +0200

    Added pin config for TTGO LoRa32 V2.1

--- src/settings.h
+++ src/settings.h
@@ -10,10 +10,24 @@
 #define UPDATE_MODBUS   2         // Modbus device is read every <n> seconds
 #define MODBUS_RETRIES  5         // no. of modbus retries
 #define INTERFACE_SEL   13        // Modbus interface select (0: USB / 1: RS485)
-#define MAX485_DE       10        // D1, DE pin on the TTL to RS485 converter
-#define MAX485_RE_NEG   2         // D2, RE pin on the TTL to RS485 converter
-#define MAX485_RX       27        // D5, RO pin on the TTL to RS485 converter
-#define MAX485_TX       9         // D6, DI pin on the TTL to RS485 converter
-#define DEBUG_TX        5
-#define DEBUG_RX        26
+#if defined(ARDUINO_TTGO_LoRa32_v21new)
+    // https://github.com/espressif/arduino-esp32/blob/master/variants/ttgo-lora32-v21new/pins_arduino.h
+    #define MAX485_DE       14        // DE pin on the TTL to RS485 converter
+    #define MAX485_RE_NEG   2         // RE pin on the TTL to RS485 converter
+    #define MAX485_RX       15        // RO pin on the TTL to RS485 converter
+    #define MAX485_TX       12        // DI pin on the TTL to RS485 converter
+    #define DEBUG_TX        00
+    #define DEBUG_RX        04
+
+#else
+    // LoRaWAN_Node board
+    // https://github.com/matthias-bs/LoRaWAN_Node
+    #define MAX485_DE       10        // D1, DE pin on the TTL to RS485 converter
+    #define MAX485_RE_NEG   2         // D2, RE pin on the TTL to RS485 converter
+    #define MAX485_RX       27        // D5, RO pin on the TTL to RS485 converter
+    #define MAX485_TX       9         // D6, DI pin on the TTL to RS485 converter
+    #define DEBUG_TX        5
+    #define DEBUG_RX        26
+    
+#endif
 #define STATUS_LED    LED_BUILTIN // Status LED
